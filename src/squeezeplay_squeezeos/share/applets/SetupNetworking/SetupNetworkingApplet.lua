7c646f6e60005b43df99368110d88ff3ae513cd9
Ralph Irving <ralph_irving@hotmail.com>
2024-12-18 16:55:49 +0000

    Enable wireless region selection during initial setup for atheros chipsets. (baby/Radio)

--- src/squeezeplay_squeezeos/share/applets/SetupNetworking/SetupNetworkingApplet.lua
+++ src/squeezeplay_squeezeos/share/applets/SetupNetworking/SetupNetworkingApplet.lua
@@ -263,9 +263,9 @@ function _wirelessRegion(self, wlan, transition)
 
 	-- skip region for atheros wlan as it supports 'world' region that
 	-- automatically configures the radio based on ap beacons
-	if wlan:isAtheros() then
-		return _connectionType(self)
-	end
+	-- if wlan:isAtheros() then
+	--	 return _connectionType(self)
+	-- end
 
 	local window = Window("text_list", self:string("NETWORK_REGION"), "setup")
 	window:setAllowScreensaver(false)
